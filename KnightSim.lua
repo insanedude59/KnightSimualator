7c504c7891b30b681d03d3a64fa4d8847c1b6149
insanedude59 <80731924+insanedude59@users.noreply.github.com>
2021-07-01 21:00:21 -0400

    Update KnightSim.lua

--- KnightSim.lua
+++ KnightSim.lua
@@ -1,4 +1,18 @@
---made by insanedude59#1000
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/IreXion-UI-Library/main/IreXion%20UI%20Library"))()
+
+local Gui = Library:AddGui({
+	Title = {"Knight Simulator", "Made by insanedude59#9329"},
+	ThemeColor = Color3.fromRGB(0, 255, 0),
+	ToggleKey = Enum.KeyCode.R,
+})
+
+local Tab = Gui:AddTab("Main")
+
+local Category = Tab:AddCategory("Autofarm")
+
+
+local Toggle = Category:AddButton("Autofarm",function()
+	   --made by insanedude59#1000
        for i,v in pairs(game.Workspace:GetDescendants()) do
     if v.Parent == game:GetService("Workspace").Enemies.Lobby then
         while v.Humanoid.Health ~= 0 do wait()
@@ -14,4 +28,5 @@
        end
        tptonpc()
        end
-           end
+           end 
+end)
