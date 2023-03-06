9a6df8b59aeba6a5ac6554937a1807aabd729104
insanedude59 <80731924+insanedude59@users.noreply.github.com>
2023-03-05 19:19:41 -0500

    Update KnightSim.lua

--- KnightSim.lua
+++ KnightSim.lua
@@ -6,39 +6,39 @@ b:Label("ALSO THERES NO AUTOEQUIP BECAUSE THE FUNCTION TO AUTO EQUIP WONT WORK")
 b:Label("SORRY")
 
 b:Toggle("Autofarm Lobby",function(bool)
-    Toggle = bool
-if Toggle == true then
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(12147.2, 342.54, -41.3032)
-end
-wait(1)
-          for i,v in pairs(game.Workspace:GetDescendants()) do
-    if v.Parent == game:GetService("Workspace").Enemies.Lobby then
-        while v.Humanoid.Health ~= 0 and Toggle == true do wait()
-           local vu = game:GetService("VirtualUser")
-            vu:ClickButton1(Vector2.new(0,0))
-        local humroot = game.Players.LocalPlayer.Character.HumanoidRootPart
-        humroot.CFrame = CFrame.new(v.HitBox.Position)
-            if v.Humanoid.Health == 0 then
-                print("health 0 guys omg")
-            end
-       end
-       end
-          end
+	Toggle = bool
+	if Toggle == true then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(12147.2, 342.54, -41.3032)
+	end
+	wait(1)
+	for i,v in pairs(game.Workspace:GetDescendants()) do
+		if v.Parent == game:GetService("Workspace").Enemies.Lobby then
+			while v.Humanoid.Health ~= 0 and Toggle == true do wait()
+				local vu = game:GetService("VirtualUser")
+				vu:ClickButton1(Vector2.new(0,0))
+				local humroot = game.Players.LocalPlayer.Character.HumanoidRootPart
+				humroot.CFrame = CFrame.new(v.HitBox.Position)
+				if v.Humanoid.Health == 0 then
+					print("health 0 guys omg")
+				end
+			end
+		end
+	end
 end)
 b:Toggle("Autofarm Desert",function(bool)
-    tog = bool
+	tog = bool
 
-          for i,v in pairs(game.Workspace:GetDescendants()) do
-    if v.Parent == game:GetService("Workspace").Enemies.Desert then
-        while v.Humanoid.Health ~= 0 and tog == true do wait()
-           local vu = game:GetService("VirtualUser")
-            vu:ClickButton1(Vector2.new(0,0))
-        local humroot = game.Players.LocalPlayer.Character.HumanoidRootPart
-        humroot.CFrame = CFrame.new(v.HitBox.Position)
-            if v.Humanoid.Health == 0 then
-                print("health 0 guys omg")
-            end
-       end
-       end
-          end
+	for i,v in pairs(game.Workspace:GetDescendants()) do
+		if v.Parent == game:GetService("Workspace").Enemies.Desert then
+			while v.Humanoid.Health ~= 0 and tog == true do wait()
+				local vu = game:GetService("VirtualUser")
+				vu:ClickButton1(Vector2.new(0,0))
+				local humroot = game.Players.LocalPlayer.Character.HumanoidRootPart
+				humroot.CFrame = CFrame.new(v.HitBox.Position)
+				if v.Humanoid.Health == 0 then
+					print("health 0 guys omg")
+				end
+			end
+		end
+	end
 end)
